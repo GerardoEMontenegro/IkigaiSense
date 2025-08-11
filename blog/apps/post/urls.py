3fe18c0b2345a76d05057f6289727d4698934315
cyruzzzz <montengr90@gmail.comgit config --global user.email montengr90@gmail.comgit config --global user.email montengr90@gmail.com>
2025-08-11 17:06:34 -0300

    Agregar validaciones y mejoras en la creación de publicaciones, incluyendo la gestión de imágenes y la verificación de contenido. Implementar el modelo de Rating y permisos de usuario tras migraciones.

--- blog/apps/post/urls.py
+++ blog/apps/post/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from apps.post import views as views
-from apps.post.views import PostCreateView, PostUpdateView, PostDeleteView
 
 app_name = 'post' # Define el espacio de nombres para las URLs de la aplicación post
 
 urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post_list'),  # Lista de posts
-    path('posts/create/', PostCreateView.as_view(), name='post_create'),   # Vista para crear un nuevo post
+    path('posts/create/', views.PostCreateView.as_view(), name='post_create'),   # Vista para crear un nuevo post
     path('posts/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),    # Detalle de un post específico
     path('posts/<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),   # Vista para eliminar un post específico
     path('posts/<slug:slug>/update/', views.PostUpdateView.as_view(), name='post_update'),   # Vista para actualizar un post específico
