23ad4a330664091cac4d35404c377aecca384dc0
cyruzzzz <montengr90@gmail.comgit config --global user.email montengr90@gmail.comgit config --global user.email montengr90@gmail.com>
2025-08-12 21:52:56 -0300

    refactor: mejorar formularios de comentarios y actualizar rutas; eliminar requerimientos innecesarios

--- blog/apps/comments/urls.py
+++ blog/apps/comments/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'comments'
 
 urlpatterns = [
-    path('comments/<uuid:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),   
+    path('comments/edit/<uuid:pk>/', views.CommentUpdateView.as_view(), name='comment_edit'),
     path('comments/<uuid:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),   
 
 ]
--- blog/apps/comments/views.py
+++ blog/apps/comments/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from apps.post.models import Comment
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from apps.comments.forms import CommentForm
 from apps.post.models import Post
@@ -17,18 +17,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
-    template_name = 'post/comment_form.html'
 
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
 
     def form_valid(self, form):
-        messages.success(self.request, "Comentario actualizado.")
+        messages.success(self.request, "Comentario actualizado con éxito.")
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
+        return reverse_lazy('post:post_detail', kwargs={'slug': self.object.post.slug})
+
+    def get(self, request, *args, **kwargs):
+        return redirect(self.get_success_url())
 
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
